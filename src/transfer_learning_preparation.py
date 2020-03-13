509a8fe490fda297a6d3f08557456fa39c8df523
Patrick Nagel <patrick.nagel@smail.inf.h-brs.de>
2020-03-13 10:01:17 +0100

    Add function which allows to freeze layers by name
    This commit adds a function which enables the user to freeze layers by their name rather than just by their index. Differentiating between freezing by index or freezing by name also the function names were set accordingly. The parameter file now also consists of a variable which is suppose to be an array of strings represting the names of the layers.

--- src/transfer_learning_preparation.py
+++ src/transfer_learning_preparation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 
-def freeze_layers(model, indices):
+def freeze_layers_by_index(model, indices):
     """Freezes the weights of layers so that they will not be changed anymore.
 
     This function freezes the weights of a model. In particular the weights of
-    the layers of a model. The model and layer 
-    information are passed as parameters. The indices point on 
+    the layers of a model. The model and layer information are passed as 
+    parameters. The indices point to the layers that will not be trainable 
+    anymore.
 
     Args:
         model (tf.keras.Model): The model of interest is passed as a parameter.
@@ -23,3 +24,21 @@ def freeze_layers(model, indices):
         for layer_index in indices:
             layer_of_interest = model.get_layer(index=layer_index)
             layer_of_interest.trainable = False
+
+
+def freeze_layers_by_name(model, names):
+    """Freezes the weights of layers so that they will not be changed anymore.
+
+    This function freezes the weights of a model. In particular the weights of
+    the layers of a model. The model and layer information are passed as 
+    parameters. The names determine the layers that will not be trainable 
+    anymore.
+
+    Args:
+        model (tf.keras.Model): The model of interest is passed as a parameter.
+        names (string array): An array of all layer names that shall be freezed.
+    """
+
+    for layer_name in names:
+        layer_of_interest = model.get_layer(name=layer_name)
+        layer_of_interest.trainable = False
